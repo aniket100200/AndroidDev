fde59dae4ba02c2dd2f818b51ada2714197c1ff4
Aniket Khangar <aniketkhangar459@gmail.com>
2026-08-02 09:39:46 +0530

    reference for Get Method

--- DAE/app/src/main/java/com/example/dae/MainActivity.java
+++ DAE/app/src/main/java/com/example/dae/MainActivity.java
@@ -32,42 +32,42 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ListView listView=findViewById(R.id.listView);
-
-        String URL="https://jsonplaceholder.typicode.com/todos/1/users";
-
-        AndroidNetworking.initialize(MainActivity.this);
-        AndroidNetworking.get(URL)
-                .setPriority(Priority.HIGH)
-                .build().getAsJSONArray(new JSONArrayRequestListener() {
-                    @Override
-                    public void onResponse(JSONArray resp) {
-                        Log.d("RES",resp.toString());
-
-                        //parsing
-                        List<String> names=new ArrayList<>();
-                        try{
-                            for(int i=0;i<resp.length();i++){
-                                JSONObject objResult = resp.getJSONObject(i);
-                                String name= objResult.getString("name");
-                                names.add(name);
-                               }
-
-                            ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,names);
-                            listView.setAdapter(arrayAdapter);
-
-                        }catch (JSONException t){
-                            t.printStackTrace();
-                        }
-
-                    }
-
-                    @Override
-                    public void onError(ANError anError) {
-                        anError.printStackTrace();
-                        Log.e("Error",anError.toString());
-                    }
-                });
+//        ListView listView=findViewById(R.id.listView);
+//
+//        String URL="https://jsonplaceholder.typicode.com/todos/1/users";
+//
+//        AndroidNetworking.initialize(MainActivity.this);
+//        AndroidNetworking.get(URL)
+//                .setPriority(Priority.HIGH)
+//                .build().getAsJSONArray(new JSONArrayRequestListener() {
+//                    @Override
+//                    public void onResponse(JSONArray resp) {
+//                        Log.d("RES",resp.toString());
+//
+//                        //parsing
+//                        List<String> names=new ArrayList<>();
+//                        try{
+//                            for(int i=0;i<resp.length();i++){
+//                                JSONObject objResult = resp.getJSONObject(i);
+//                                String name= objResult.getString("name");
+//                                names.add(name);
+//                               }
+//
+//                            ArrayAdapter<String>arrayAdapter=new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,names);
+//                            listView.setAdapter(arrayAdapter);
+//
+//                        }catch (JSONException t){
+//                            t.printStackTrace();
+//                        }
+//
+//                    }
+//
+//                    @Override
+//                    public void onError(ANError anError) {
+//                        anError.printStackTrace();
+//                        Log.e("Error",anError.toString());
+//                    }
+//                });
 
 
     }
