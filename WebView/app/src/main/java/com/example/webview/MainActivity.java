87aacba12841932531e3276c54ad0e0664510a01
Aniket Khangar <aniketkhangar459@gmail.com>
2026-08-02 11:11:55 +0530

    Web View

--- WebView/app/src/main/java/com/example/webview/MainActivity.java
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -1,27 +1,66 @@
 package com.example.webview;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private ProgressBar pgBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         webView=findViewById(R.id.webView);
+        pgBar=findViewById(R.id.pgBar);
+
+        // 1. Enable JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
 
         webView.loadUrl("https://www.google.com");
 
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
 
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                pgBar.setVisibility(WebView.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                pgBar.setVisibility(WebView.GONE);
+            }
+        });
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if(webView.canGoBack()){
+                    webView.goBack();
+                }else{
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                }
+            }
+        });
 
     }
+
+
 }
